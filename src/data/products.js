021638da9426fc1a2261e15977cc6ca541b8b95a
Vitaliy Pshegorskiy <pshegorsky@gmail.com>
2022-02-19 22:13:04 +0600

    4.4 Атрибут key для списка

--- src/data/products.js
+++ src/data/products.js
@@ -1,45 +1,54 @@
 export default [
   {
+    id: 1,
     title: 'HALO Фигурка героя Brute Captain 3.75" с аксессуарами',
     price: 1299,
     image: '/img/HLW0006.jpg',
   },
   {
+    id: 2,
     title: 'HALO Фигурка героя Spartan MK. VII 3.75" с аксессуарами',
     price: 1299,
     image: '/img/HLW0007.jpg',
   },
   {
+    id: 3,
     title: 'HALO Фигурка героя H&V-1 3.75" с аксессуарами',
     price: 2690,
     image: '/img/HLW0009.jpg',
   },
   {
+    id: 4,
     title: 'Fortnite Фигурка героя Ruin с аксессуарами',
     price: 2190,
     image: '/img/FNT0284.jpg',
   },
   {
+    id: 5,
     title: 'Fortnite Фигурка Bone Wasp с аксессуарами',
     price: 3590,
     image: '/img/FNT0604.jpg',
   },
   {
+    id: 6,
     title: 'Fortnite Фигурка Omega - Purple с аксессуарами',
     price: 3590,
     image: '/img/FNT0237.jpg',
   },
   {
+    id: 7,
     title: 'HALO Фигурка героя Jackal Sniper 3.75" с аксессуарами',
     price: 1299,
     image: '/img/HLW0005.jpg',
   },
   {
+    id: 8,
     title: 'HALO Фигурка героя MP Armor A 6.5" с аксессуарами',
     price: 2199,
     image: '/img/HLW0020.jpg',
   },
   {
+    id: 9,
     title: 'Fortnite Фигурка героя Archetype с аксессуарами',
     price: 1199,
     image: '/img/FNT0260.jpg',
