da9f1fb1beac3450fcfee2cd99b9e6a327a116f7
Fozzyk <1pvg@mail.ru>
2022-03-10 18:22:33 +0600

    q

--- src/main.js
+++ src/main.js
@@ -3,15 +3,11 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
-import data from './data';
-
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  data,
+
   render: (h) => h(App),
 }).$mount('#app');
-
-alert(data.message);
