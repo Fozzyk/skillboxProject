f638f14a8aa0af4491ae9743085198503bc39dcd
Fozzyk <1pvg@mail.ru>
2022-03-24 20:57:59 +0600

    8.1 Форма оформления заказа. Слоты и примеси

---
+++ src/components/OrderItem.vue
@@ -0,0 +1,27 @@
+<template>
+            <li class="cart__order">
+              <h3>{{ item.product.title }}</h3>
+              <b>{{ (item.amount * item.product.price) | numberFormat }} ₽</b>
+              <span>Артикул: {{ item.product.id }}</span>
+              <span>Количество: {{ item.amount }}</span>
+            </li>
+</template>
+
+<script>
+import numberFormat from '@/helpers/numberFormat';
+
+export default {
+  filters: { numberFormat },
+  props: ['item'],
+  computed: {
+    amount: {
+      get() {
+        return this.item.amount;
+      },
+      set(value) {
+        this.$store.dispatch({ productId: this.item.productId, amount: value });
+      },
+    },
+  },
+};
+</script>
